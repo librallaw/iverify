527fb11fd9fda5a2086a46d9d4810823c470dd00
Excellent Lawrence <librallaw@Excellent-Lawrence.local>
2023-01-29 22:08:39 +0100

    Add create users

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -297,15 +297,29 @@ class VerificationController extends Controller
 
 
 
-               // dd($data);
+              //  dd($data);
 
 
 
 
                 if(isset($data->data->data)){
 
-                    $walletBalance->balance = $walletBalance->balance - 1;
-                    $walletBalance->save();
+                    if(is_object($data->data->data)){
+
+
+                        $log = new Accountlog();
+                        $log->unique_id = Auth::user()->unique_id;
+                        $log->action = "Verify";
+                        $log->trigger =  Auth::user()->unique_id;
+                        $log->message = "Empty Data <-> Demo <->$request->first_name <-> $request->last_name <-> $request->dob <-> $request->gender";
+                        $log->status = 0;
+                        $log ->save();
+
+
+                        return Redirect::back()->with("message", "User has no record on file, no further action required")->with("type",'danger');
+                    }
+
+
                 }
 
 
@@ -316,6 +330,9 @@ class VerificationController extends Controller
 
 
                 if($data->status){
+
+                    $walletBalance->balance = $walletBalance->balance - 1;
+                    $walletBalance->save();
 
 
                     $log = new Accountlog();
